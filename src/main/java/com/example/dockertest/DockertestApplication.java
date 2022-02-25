bd8b16d656c6154dcfe5be180dade4af204ee5eb
SalimAbdala27 <salimabdala27@hotmail.com>
2022-02-25 12:15:33 +0000

    added basic entry point

---
+++ src/main/java/com/example/dockertest/DockertestApplication.java
@@ -0,0 +1,21 @@
+package com.example.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DockertestApplication {
+
+	@GetMapping("/")
+	public String sayHello(){
+		return "Say Hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertestApplication.class, args);
+	}
+
+}
